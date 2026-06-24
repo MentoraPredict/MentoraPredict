3603d9134159b076ebb65261c4e92bd7ef4d1048
Alex-Xav-Ch <alex.xavier.sisu26@gmail.com>
2026-06-24 11:51:12 -0500

    fix(backend): enable passWithNoTests to unblock CI build into prediction-service

--- services/prediction-service/jest.config.js
+++ services/prediction-service/jest.config.js
@@ -1,19 +1,26 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
-  testRegex: '.*\\.spec\\.ts$',
+  moduleFileExtensions: ["js", "json", "ts"],
+  rootDir: "src",
+  testRegex: ".*\\.spec\\.ts$",
+
   transform: {
-    '^.+\\.(t|j)s$': ['ts-jest', {
-      tsconfig: {
-        experimentalDecorators: true,
-        emitDecoratorMetadata: true,
-        strict: true,
-        esModuleInterop: true,
-        skipLibCheck: true,
-        types: ['jest', 'node'],
+    "^.+\\.(t|j)s$": [
+      "ts-jest",
+      {
+        tsconfig: {
+          experimentalDecorators: true,
+          emitDecoratorMetadata: true,
+          strict: true,
+          esModuleInterop: true,
+          skipLibCheck: true,
+          types: ["jest", "node"],
+        },
       },
-    }],
+    ],
   },
-  coverageDirectory: '../coverage',
-  testEnvironment: 'node',
+
+  coverageDirectory: "../coverage",
+  testEnvironment: "node",
+
+  passWithNoTests: true,
 };
